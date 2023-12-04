b82f84f9a976b075d2972cda01e735db119a2c14
Enzo <e.lebrun@qantis.co>
2023-12-04 16:54:59 +0100

    adding scrapping code

---
+++ scrapping/__init__.py
@@ -0,0 +1,11 @@
+from scrapping.cars.aramis import AramisScrapper
+from scrapping.cars.autosphere import AutoSphereScrapper
+from scrapping.cars.heycar import HeyCarScrapper
+from scrapping.cars.autohero import AutoHeroScrapper
+
+SCRAPPERS = [
+    AramisScrapper,
+    AutoSphereScrapper,
+    HeyCarScrapper,
+    AutoHeroScrapper,
+]
